6fdcefbe9fc051cff748f8c7d4043644da1243dd
lsh1752 <leesunghyuck0@gmail.com>
2025-05-13 21:37:06 +0900

    init: 초기화

---
+++ src/day01/Main.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");                    //sout: 콘솔에 문자열 출력
+    }
+}
+//: 한 줄 주석 처리  or  해당 줄에서 Ctrl+/
+
