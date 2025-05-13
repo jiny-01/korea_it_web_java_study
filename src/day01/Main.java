4d1aabdfcf82201445f89e910684352af9252b8c
jiny-01 <allie7019@naver.com>
2025-05-13 21:46:21 +0900

    init: first commit

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -2,7 +2,8 @@ package day01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");                    //sout: 콘솔에 문자열 출력
+        System.out.println("Hello");
+        System.out.println("world");           //sout: 콘솔에 문자열 출력
     }
 }
 //: 한 줄 주석 처리  or  해당 줄에서 Ctrl+/
